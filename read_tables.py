cc2683107b4731b52815ccca444a5d2609f09e32
Nevermind884 <cordeliajcole99@outlook.com>
2026-04-04 23:04:54 +0100

    Away team, Home team and stadium extracted

--- read_tables.py
+++ read_tables.py
@@ -14,6 +14,8 @@ def getTable():
         #print(f"Found {len(all_tables)} tables")
 
         getMatchScore(url)
+        getHomeAwayTeam(url)
+        getStadium(url)
         break
         
 
@@ -22,3 +24,42 @@ def getMatchScore(url):
     results = version_tables[1]["G A:B"]
     total = results.dropna().iloc[-1]  
     print(total)
+
+def getHomeAwayTeam(url):
+    version_tables = imp.pd.read_html(url, match='Home')
+
+    gameData = version_tables[2].head(2)
+    home_team = gameData.iloc[0, 2]
+    
+    #Removing Home before team name 
+    stringArray = home_team.split()
+    team = []
+    for i in stringArray:
+        if i != 'Home':
+            team.append(i)
+            home = " ".join(team)
+    print(home)
+    away_team = gameData.iloc[0, 3]
+
+    #Removing Visitor before team name 
+    stringArray = away_team.split()
+    team = []
+    for i in stringArray:
+        if i != 'Visitor':
+            team.append(i)
+            away = " ".join(team)
+    print(away)
+
+def getStadium(url):
+    version_tables = imp.pd.read_html(url, match='Home')
+    gameData = version_tables[2].head(2)
+    location = gameData.iloc[1, 2]
+
+    #Removing Palce before stadium name 
+    stringArray = location.split()
+    loc = []
+    for i in stringArray:
+        if i != 'Place':
+            loc.append(i)
+            stadium = " ".join(loc)
+    print(stadium)
