21205377cfdd38f92a2cd21cad50aab7853ea397
Nevermind884 <cordeliajcole99@outlook.com>
2026-07-20 12:13:43 +0100

    Added new pages and updated show/load page functions

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import ttkbootstrap as ttk
 from appState import AppState
 from Database import team_queries as tq
+from Database import league_queries as lq
+from Database import match_queries as mq
+from datetime import datetime
 
 class MyApp(ttk.Frame):
 
@@ -24,10 +27,12 @@ class MyApp(ttk.Frame):
 
         from Views.home import Home
         from Views.query_results import SearchResults
+        from Views.view_team_stats import TeamStats
 
         pages = {
             "home": Home,
             "search results": SearchResults,
+            "view team stats": TeamStats
         }
 
         for name, Page in pages.items():
@@ -38,10 +43,41 @@ class MyApp(ttk.Frame):
 
 
     def show_page(self, name):
+        page = self.pages[name]
 
-        self.pages[name].tkraise()
+        if hasattr(page, "refresh"):
+            page.refresh()
+
+        page.tkraise()
 
     def perform_search(self, query):
         self.state.search_query = query
         self.state.search_results = tq.searchTeamName(query)
-        self.show_page("search results")    
+        #print(self.state.search_results) #Debug
+        self.show_page("search results")
+
+    def load_team_stats(self,team_name):
+        self.state.current_league = lq.getcurrLeague()
+
+        self.state.num_league_matches = tq.teamMatchesInLeagueCount(
+            self.state.current_league,
+            team_name
+        )
+        print(str(self.state.num_league_matches))
+
+        self.state.home_wins = mq.homeWinCount(
+            self.state.current_league,
+            team_name
+        )
+        print(str(self.state.home_wins))
+
+        self.state.away_wins = mq.awayWinCount(
+            self.state.current_league,
+            team_name
+        )
+        print(str(self.state.away_wins))
+        
+        self.show_page("view team stats")
+
+        #if end year is this year and match happened before may th and after august the prior year
+        #2025 after august - 2026 may, if curr date before 2026 sep then 2025/26 league
