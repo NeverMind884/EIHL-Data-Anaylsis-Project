28f616699460e4a359de92c72b2c08be168e05ce
Nevermind884 <cordeliajcole99@outlook.com>
2026-07-20 12:25:58 +0100

    test/example queries

---
+++ SQL Queries/INSERT_LEAGUES.sql
@@ -0,0 +1,6 @@
+Use eihl;
+
+INSERT INTO tournament 
+    (league_name, league_start_date, league_end_date)
+VALUES
+    ('EIHL 2026/27', '2026-09-01', '2027-04-18');
---
+++ SQL Queries/UPDATE_LEAGUE.sql
@@ -0,0 +1,11 @@
+use eihl;
+
+UPDATE tournament
+SET league_start_date = '2025-09-13'
+WHERE league_id = 1;
+
+use eihl;
+
+UPDATE tournament
+SET league_end_date = '2026-04-06'
+WHERE league_id = 1;
